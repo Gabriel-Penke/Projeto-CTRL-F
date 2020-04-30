7cdd251dc77f52e0f84e115ae45150bcefbcb56a
Gabriel-Penke <gabri.penke@gmail.com>
2020-04-30 14:37:13 -0300

    Alterações 30/04/2020
    Adicionei algumas linhas de comentário no arquivo index.js e  criei um estilo pela tag style no arquivo index.html

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ function searchWord(){
     const text = document.getElementById('txtArea').value;
     const check = document.getElementById('check');
     const result = document.getElementById('result');
-    let pattern = verificationCase(check, word);
+    let pattern = verificationCase(check, word);    //Função para verificar se é case insensitive
     
-    if(pattern.test(text)){
-        console.log(text.match(pattern));
+    if(pattern.test(text)){     //Verifica se a palavra está no texto
+        console.log(text.match(pattern));   //Mostra um array com a palvra no console
         result.innerHTML = "'" + word + "'" + ' first occurrence in position ' + text.search(pattern) + '<br>'
         + text.match(pattern).length + ' occurrences in text';
     }else{
-        result.innerHTML = 'Not founded';
+        result.innerHTML = 'Not founded';   //Caso a palavra não conste no texto
     }   
 
-    document.getElementById('inputElement').value = '';    
+    document.getElementById('inputElement').value = '';    //Devolve um valor vazio para o input
 }
 
-function verificationCase(check, word){
+function verificationCase(check, word){     //Verifica se é case insensitive
     if(check.checked) return new RegExp(word, 'gim');
     else return new RegExp(word, 'g');
 }
